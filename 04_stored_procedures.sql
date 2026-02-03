075b07f975b3dde6cfe57540c194a70f4a5f6657
omannnn9 <dilloooman26@gmail.com>
2026-02-03 18:23:33 +0400

    Update 04_stored_procedures.sql

--- 04_stored_procedures.sql
+++ 04_stored_procedures.sql
@@ -11,15 +11,23 @@ BEGIN
     VALUES (@ProductName, @CategoryID, @SupplierID, @UnitPrice, @ReorderLevel);
 END;
 
---2 update product price 
+--2 update product price with try catch if product does not exist
 CREATE PROCEDURE sp_UpdateProductPrice
     @ProductID INT,
     @NewPrice DECIMAL(10,2)
 AS
 BEGIN
-    UPDATE Product
-    SET UnitPrice = @NewPrice
-    WHERE ProductID = @ProductID;
+    BEGIN TRY
+        UPDATE Product
+        SET UnitPrice = @NewPrice
+        WHERE ProductID = @ProductID;
+
+        IF @@ROWCOUNT = 0
+            PRINT 'Product does not exist';
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error occurred';
+    END CATCH
 END;
 
 --3 view all products & aupplier
