b0c9ea688705fb9c098de65446612dd6c2959cc2
omannnn9 <dilloooman26@gmail.com>
2026-02-04 17:59:02 +0400

    Update 05_triggers.sql

--- 05_triggers.sql
+++ 05_triggers.sql
@@ -1,14 +1,1 @@
---1 update stock after sale 
-
-CREATE TRIGGER trg_UpdateStockAfterSale
-ON Sale
-AFTER INSERT
-AS
-BEGIN
-    UPDATE Stock
-    SET Quantity = Quantity - i.QuantitySold,
-        LastUpdated = GETDATE()
-    FROM Stock s
-    INNER JOIN inserted i ON s.ProductID = i.ProductID;
-END;
 
