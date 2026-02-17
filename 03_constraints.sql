7c3c43b30761558e3683778377062a3f4b4ac270
omannnn9 <dilloooman26@gmail.com>
2026-02-17 19:38:52 +0400

    Update 03_constraints.sql

--- 03_constraints.sql
+++ 03_constraints.sql
@@ -1,8 +1,5 @@
---1 price must be positive
-CREATE RULE Rule_PositivePrice AS @Price > 0;
-EXEC sp_bindrule 'Rule_PositivePrice', 'Product.UnitPrice';
 
---2 prevent duplicate customers by phone number 
+-- prevent duplicate customers by phone number 
  ALTER TABLE Customer
 ADD CONSTRAINT UQ_Customer_Phone UNIQUE (Phone);
 
