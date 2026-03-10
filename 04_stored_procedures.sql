192a1b2b33fff77721fb4b1fb2c677cf7c086ae0
omannnn9 <dilloooman26@gmail.com>
2026-03-10 11:08:53 +0400

    Update 04_stored_procedures.sql

--- 04_stored_procedures.sql
+++ 04_stored_procedures.sql
@@ -9,7 +9,7 @@ AS
 BEGIN
     BEGIN TRY
      INSERT INTO Product (ProductName, CategoryID, SupplierID, UnitPrice, ReorderLevel)
-     VALUES (@ProductName, @CategoryID, @SupplierID, @UnitPrice, @ReorderLevel);
+     VALUES (@ProductName, @CategoryID, @SupplierID, @UnitPrice, @ReorderLevel)
     END TRY
     BEGIN CATCH
         PRINT 'ERROR' + ERROR_MESSAGE()
@@ -25,10 +25,10 @@ BEGIN
     BEGIN TRY
         UPDATE Product
         SET UnitPrice = @NewPrice
-        WHERE ProductID = @ProductID;
+        WHERE ProductID = @ProductID
 
         IF @@ROWCOUNT = 0
-            PRINT 'Product does not exist';
+            PRINT 'Product does not exist'
     END TRY
     BEGIN CATCH
         PRINT 'Error' + ERROR_MESSAGE()
@@ -43,7 +43,7 @@ BEGIN
      SELECT p.ProductID, p.ProductName, c.CategoryName, s.SupplierName, p.UnitPrice
      FROM Product p
      JOIN Category c ON p.CategoryID = c.CategoryID
-     JOIN Supplier s ON p.SupplierID = s.SupplierID;
+     JOIN Supplier s ON p.SupplierID = s.SupplierID
     END TRY
     BEGIN CATCH
         PRINT 'Error' + ERROR_MESSAGE()
